dd56ffedee25898a6a03985359a1f0b4c7281829
QuenUI <quen-ui@gmail.com>
2025-06-11 08:49:55 +0700

    docs: Added documentation for component Card

--- scripts/docgen/docgenPaths.ts
+++ scripts/docgen/docgenPaths.ts
@@ -2,5 +2,7 @@ export const PATHS: string[] = [
   "./packages/components/src/Button/Button.tsx",
   "./packages/components/src/Alert/Alert.tsx",
   "./packages/components/src/Avatar/Avatar.tsx",
-  "./packages/components/src/Badge/Badge.tsx"
+  "./packages/components/src/Badge/Badge.tsx",
+  "./packages/components/src/Breadcrumbs/Breadcrumbs.tsx",
+  "./packages/components/src/Card/Card.tsx"
 ];

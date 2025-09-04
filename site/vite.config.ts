ff4836c9fc8c0ac8bfdb3aac25fd2eb92507bd41
QuenUI <quen-ui@gmail.com>
2025-09-04 13:48:53 +0700

    build: Fixed settings vite for site

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     react(),
     svgr(),
     mdx({
+      providerImportSource: "@mdx-js/react",
       remarkPlugins: [
         remarkFrontmatter,
         remarkMdxFrontmatter,

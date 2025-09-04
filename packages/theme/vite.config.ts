621a060e0af15923b63affc7da67f0865cf98bea
QuenUI <quen-ui@gmail.com>
2025-09-04 15:05:16 +0700

    build: Fixed build theme package

--- packages/theme/vite.config.ts
+++ packages/theme/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: "tsconfig.app.json"
+    })
+  ],
+  build: {
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-dom"],
+      output: {
+        entryFileNames: "[name].js",
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+          "react/jsx-runtime": "react/jsx-runtime"
+        }
+      }
+    },
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      formats: ["es"]
+    }
+  }
+});

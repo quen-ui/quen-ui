0d2cf792c6e6d233cacddd129d69670eaeb4d324
QuenUI <quen-ui@gmail.com>
2025-09-09 18:42:19 +0700

    docs: Added documentation for component Text

--- packages/components/src/typography/Text/types.ts
+++ packages/components/src/typography/Text/types.ts
@@ -13,12 +13,19 @@ export type TTextSize = (typeof TEXT_SIZE)[number];
 export type TTextType = (typeof TEXT_TYPE)[number];
 
 export interface ITextProps {
+  /** Controls text size */
   size?: TTextSize;
+  /** Custom CSS color value (overrides type) */
   color?: string;
+  /** Click event handler */
   onClick?: MouseEventHandler<HTMLElement>;
+  /** Semantic state style */
   type?: TTextType;
+  /** HTML tag to render */
   as?: keyof JSX.IntrinsicElements;
+  /** Custom CSS class */
   className?: string;
+  /** Inline styles */
   styles?: CSSProperties
   [key: string]: any;
 }

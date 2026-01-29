760100909ba8a2d9bce8138a77f2989a1bb014c5
QuenUI <quen-ui@gmail.com>
2026-01-29 13:36:44 +0700

    feat(component): Added variants contents for Inputs

--- packages/components/src/InputBase/InputBase.tsx
+++ packages/components/src/InputBase/InputBase.tsx
@@ -1,6 +1,12 @@
-import React, { PropsWithChildren } from "react";
+import { type PropsWithChildren, useMemo } from "react";
 import type { IInputBaseProps } from "./types";
-import { InputBaseWrapper, InputBaseContainer } from "./styles";
+import {
+  InputBaseWrapper,
+  InputBaseContainer,
+  InputContentText,
+  InputBaseAddonWrapper,
+  InputBaseRightAddonWrapper
+} from "./styles";
 import { Text } from "../typography/Text";
 
 const InputBase = ({
@@ -15,20 +21,54 @@ const InputBase = ({
   children,
   leftContent,
   rightContent,
+  leftContentVariant = "icon",
+  rightContentVariant = "icon",
   ...props
 }: PropsWithChildren<IInputBaseProps>) => {
+  const leftContentRender = useMemo(() => {
+    if (leftContent) {
+      if (leftContentVariant === "text") {
+        return <InputContentText size={size}>{leftContent}</InputContentText>;
+      } else if (leftContentVariant === "addon") {
+        return <InputBaseAddonWrapper>{leftContent}</InputBaseAddonWrapper>;
+      }
+      return leftContent;
+    }
+    return null;
+  }, [leftContent, leftContentVariant, size]);
+
+  const rightContentRender = useMemo(() => {
+    if (rightContent) {
+      if (rightContentVariant === "text") {
+        return <InputContentText size={size}>{rightContent}</InputContentText>;
+      } else if (rightContentVariant === "addon") {
+        return <InputBaseRightAddonWrapper>{rightContent}</InputBaseRightAddonWrapper>;
+      }
+      return rightContent;
+    }
+    return null;
+  }, [rightContent, rightContentVariant, size]);
+
   return (
-    <InputBaseWrapper className={className} style={style} {...props}>
+    <InputBaseWrapper
+      className={className}
+      style={style}
+      {...props}
+      size={size}>
       {label && (
         <Text size={size} as="label" htmlFor={id}>
           {label}
           {required && <span className="quen-ui__input-base--required">*</span>}
         </Text>
       )}
-      <InputBaseContainer size={size} error={error} disabled={disabled}>
-        {leftContent}
+      <InputBaseContainer
+        size={size}
+        error={error}
+        disabled={disabled}
+        leftContent={!!leftContent && leftContentVariant !== "icon"}>
+        {leftContentRender}
         {children}
-        {rightContent}
+        {rightContentRender}
       </InputBaseContainer>
       {typeof error === "string" && (
         <Text className="quen-ui__input-base--error-message" size="xs">
--- packages/components/src/TextField/__stories__/TextField.stories.tsx
+++ packages/components/src/TextField/__stories__/TextField.stories.tsx
@@ -1,6 +1,9 @@
 import { StoryObj } from "@storybook/react";
+import { IconSearch } from "@tabler/icons-react";
 import TextField from "../TextField";
 import { QUEN_SIZE } from "../../constants";
+import { Select } from "../../Select";
+import { Button } from "../../Button";
 
 export default {
   title: "Components/TextField",
@@ -13,10 +16,40 @@ export default {
     disabled: { control: "boolean" },
     label: { control: "text", defaultValue: "Label" },
     error: { control: "text" },
+    leftContentVariant: { control: "select", options: ["icon", "text", "addon"] },
   }
 } as StoryObj<typeof TextField>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Example = {
 } as StoryObj<typeof TextField>;
+
+export const ExampleLeftContentSelect = {
+  args: {
+    leftContent: <Select items={[{ label: "1", value: "1"}]} />,
+    leftContentVariant: "addon"
+    }
+} as StoryObj<typeof TextField>;
+
+export const ExampleLeftContentText = {
+  args: {
+    leftContent: "http://",
+    leftContentVariant: "text",
+  }
+} as StoryObj<typeof TextField>;
+
+export const ExampleLeftContentIcon = {
+  args: {
+    leftContent: <IconSearch />,
+    leftContentVariant: "icon"
+  }
+} as StoryObj<typeof TextField>;
+
+export const ExampleRightContentButton = {
+  args: {
+    rightContent: <Button>Search</Button>,
+    rightContentVariant: "addon"
+  }
+} as StoryObj<typeof TextField>;
+
 
